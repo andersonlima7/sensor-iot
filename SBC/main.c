e570fa7204baa5e3603793bcbb37f254f591933c
Anderson Lima <andersonlll007@gmail.com>
2022-12-17 15:12:53 -0300

    Corrigido errado de desconexão

--- SBC/main.c
+++ SBC/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-// #include <wiringPi.h>
-// #include <lcd.h>
+#include <wiringPi.h>
+#include <lcd.h>
 #include <MQTTClient.h>
 #include <stdlib.h>
 #include <string.h>
